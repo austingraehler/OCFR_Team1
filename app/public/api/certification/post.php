3ea66a6f7e353cd3694430e674839bedde88271f
Hayley Madura <hmadura@iu.edu>
2019-10-16 14:56:31 -0400

    add certificiation apis - index and post

---
+++ app/public/api/certification/post.php
@@ -0,0 +1,28 @@
+<?php
+use Ramsey\Uuid\Uuid;
+
+// Step 0: Validate data
+
+// Step 1: Get a datase connection from our help class
+$db = DbConnection::getConnection();
+
+// Step 2: Prepare & run the query
+$stmt = $db->prepare(
+'INSERT INTO Certification
+  (certificationID, agency, certificationName, standardExpiry)
+  VALUES (?,?,?,?)'
+);
+
+
+$certificationID = Uuid::uuid4()->toString();
+
+$stmt->execute([
+  $certificationID, // i.e. 25769c6c-d34d-4bfe-ba98-e0ee856f3e7a
+  $_POST['agency'],
+  $_POST['certificationName'],
+  $_POST['standardExpiry'],
+]);
+
+// Step 4: Output
+header('HTTP/1.1 303 See Other');
+header('Location: ../records/?certificationID='.certificationID);
