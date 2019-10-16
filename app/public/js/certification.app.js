8595ad6f0cb421e106fc525d79fcc8e6a4710669
Austin Graehler <jgraehle@iu.edu>
2019-10-16 11:26:36 -0400

    creation of js files

---
+++ app/public/js/certification.app.js
@@ -0,0 +1,53 @@
+var certificationApp = new Vue({
+  el: '#memberCertificationsApp',
+  data: {
+    personID: [],
+    firstName: {},
+    lastName: {
+    address: '',
+    email: [],
+    phoneNumber: [],
+    dob: '',
+    startDate: '',
+    gender: '',
+    }
+  },
+  methods: {
+    fetchClient() {
+      fetch('api/records/')
+      .then(response => response.json())
+      .then(json => { patientRecordsApp.patients = json })
+    },
+    handleSubmit(event) {
+      fetch('api/records/post.php', {
+        method:'POST',
+        body: JSON.stringify(this.recordPatient),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      })
+      .then( response => response.json() )
+      .then( json => { patientRecordsApp.patients.push(json[0]) })
+      .catch( err => {
+        console.error('RECORD POST ERROR:');
+        console.error(err);
+      })
+      this.handleReset();
+    },
+    handleReset() {
+      this.recordPatient = {
+        firstName: '',
+        lastName: '',
+        dob: '',
+        sexAtBirth: ''
+      }
+    },
+    handleRowClick(patient) {
+      patientTriageApp.patient = patient;
+    }
+  }, // end methods
+  created() {
+    this.handleReset();
+    this.fetchPatients();
+  }
+});
