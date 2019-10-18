38741a2843c9554af7bc14da04170f57e7e130a8
Austin Graehler <jgraehle@iu.edu>
2019-10-18 12:44:27 -0400

    Table information grab

--- app/public/js/certification.app.js
+++ app/public/js/certification.app.js
@@ -1,17 +1,22 @@
 var certificationApp = new Vue({
   el: '#certificationApp',
   data: {
-    certificationID: [],
-    agency: {},
-    certificationName: {},
-    standardExpiry: {},
-    certifications: []
+    certifications: [],
+    formData: {
+        cid: '',
+        agency: '',
+        name: '',
+        expiry: ''
+    }
   },
   methods: {
     fetchCertifications() {
       fetch('api/certification/index.php')
       .then(response => response.json())
       .then(json => { certificationApp.certifications = json });
+    },
+    handleSubmit(evt) {
+
     }
   }, // end methods
   created() {
