702b9e8bf95921d7376ca6abccfcb1fe6d36ba20
tsaiyc <tsaiyu@iu.edu>
2019-10-19 14:48:16 -0400

    Split two js files

--- app/public/js/certification.app.js
+++ app/public/js/certification.app.js
@@ -13,37 +13,13 @@ var certificationApp = new Vue({
     fetchCertifications() {
       fetch('api/certification/index.php')
       .then(response => response.json())
-      .then(json => { certificationApp.certifications = json })
+      .then(json => { certificationApp.certifications = json });
     },
-    handleSubmit(event) {
-      fetch('api/certification/post.php', {
-        method:'POST',
-        body: JSON.stringify(this.formData),
-        headers: {
-          "Content-Type": "application/json; charset=utf-8"
-        }
-      })
-      .then( response => response.json() )
-      .then( json => { certForm.formData.push(json[0]) })
-      .catch( err => {
-        console.error('Certification Post ERROR:');
-        console.error(err);
-      })
-      this.handleReset();
-    },
-    handleReset() {
-      this.formData = {
-        certificationID: '',
-        agency: '',
-        certificationName: '',
-        standardExpiry: ''
-      }
-    },
-    // end methods
+    handleSubmit(evt) {
+
+    }
+  }, // end methods
   created() {
     this.fetchCertifications();
-    this.handleSubmit();
-    this.handleReset();
   }
-}
 });
