87181e9f8ef97c15a3c58ce9d79621e2db3c74c7
cnboyer <cnboyer@iu.edu>
2019-10-24 20:29:30 -0400

    Update PHP Files Created

---
+++ app/public/api/certification/update.php
@@ -0,0 +1,30 @@
+<?php
+
+//DB connection
+$db = DbConnection::getConnection();
+
+if (isset($_POST['certificationID']) && $_POST['certificationID']!='') {
+  $stmt = $db->prepare(
+    'UPDATE certificationApp set certificationID = ?, agency = ?, certificationName = ?, standardExpiry = ?
+    where certificationID = ?'
+  );
+  $stmt->execute([
+    $_POST['agency'],
+    $_POST['certificationName'],
+    $_POST['standardExpiry']
+  ]);
+}
+ else {
+   $stmt = $db ->prepare(
+     'INSERT INTO certificationApp
+     (agency, certificationName, standardExpiry)
+     VALUES (?, ?, ?)'
+   );
+
+   $stmt->execute([
+     $_POST['agency'],
+     $_POST['certificationName'],
+     $_POST['standardExpiry']
+   ]);
+
+ }
